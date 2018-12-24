ecab1effa8ebce4a142ae1035a33d623674b3422
tianjihai <jihai123@vip.qq.com>
2018-12-24 20:39:53 +0800

    second page

---
+++ src/second/SecondPage.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div>
+    This is secondPage!!
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "SecondPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/second/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by
+ * @Auther: tianjihai
+ * @Date: 2018-12-24 18:41
+ */
+
+import Second from './SecondPage'
+
+export default Second;
